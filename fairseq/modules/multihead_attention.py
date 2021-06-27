d0a2c609680156df0da2a5db77e35663effd6002
Zhong-Zhang <zhongzhang.me@gmail.com>
2021-06-27 14:03:36 +0800

    test

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -414,6 +414,7 @@ class MultiheadAttention(nn.Module):
         attn_weights = torch.bmm(q, k.transpose(1, 2))
         attn_weights = MultiheadAttention.apply_sparse_mask(attn_weights, tgt_len, src_len, bsz)
 
+
         assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]
 
         if attn_mask is not None:
